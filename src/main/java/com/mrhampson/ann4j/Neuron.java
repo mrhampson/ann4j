b12f52f7b847ab777706c8e60004deac60708c3d
Marshall Hampson <mhampson@liveaction.com>
2018-10-05 11:31:55 -0700

    Clean up neuron construction

--- src/main/java/com/mrhampson/ann4j/Neuron.java
+++ src/main/java/com/mrhampson/ann4j/Neuron.java
@@ -2,7 +2,10 @@ package com.mrhampson.ann4j;
 
 import com.mrhampson.ann4j.utils.Pair;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import java.util.function.Supplier;
 
 /**
@@ -12,11 +15,9 @@ public class Neuron {
   private final List<Pair<Supplier<Double>, Double>> inputs;
   private final ActivationFunction activationFunction;
   
-  public Neuron(
-    ActivationFunction activationFunction,
-    List<Pair<Supplier<Double>, Double>> inputs) {
-    this.activationFunction = activationFunction;
-    this.inputs = inputs;
+  private Neuron(Builder builder) {
+    this.activationFunction = builder.activationFunction;
+    this.inputs = Collections.unmodifiableList(builder.inputs);
   }
   
   public double getOutput() {
@@ -24,5 +25,32 @@ public class Neuron {
       .mapToDouble(input -> input.getFirst().get() * input.getSecond())
       .sum();
     return activationFunction.apply(sum);
+  }
+  
+  public static final class Builder {
+    private ActivationFunction activationFunction;
+    private final List<Pair<Supplier<Double>, Double>> inputs = new ArrayList<>();
+    
+    public Builder takesInput(Neuron neuron, double weight) {
+      Objects.requireNonNull(neuron);
+      this.inputs.add(Pair.of(neuron::getOutput, weight));
+      return this;
+    }
+    
+    public Builder takesInput(Supplier<Double> input, double weight) {
+      Objects.requireNonNull(input);
+      this.inputs.add(Pair.of(input, weight));
+      return this;
+    }
+    
+    public Builder withActivationFunction(ActivationFunction activationFunction) {
+      Objects.requireNonNull(activationFunction);
+      this.activationFunction = activationFunction;
+      return this;
+    }
+    
+    public Neuron build() {
+      return new Neuron(this);
+    }
   }
 }
--- src/main/java/com/mrhampson/ann4j/TestMain.java
+++ src/main/java/com/mrhampson/ann4j/TestMain.java
@@ -15,23 +15,30 @@ public class TestMain {
     Supplier<Double> input1 = () -> 1d;
     Supplier<Double> input2 = () -> 1d;
     
-    Neuron hidden1 = new Neuron(
-      ActivationFunctions::sigmoid, 
-      Arrays.asList(Pair.of(input1, 0.8), Pair.of(input2, 0.2))
-    );
-    Neuron hidden2 = new Neuron(
-      ActivationFunctions::sigmoid,
-      Arrays.asList(Pair.of(input1, 0.4), Pair.of(input2, 0.9))
-    );
-    Neuron hidden3 = new Neuron(
-      ActivationFunctions::sigmoid,
-      Arrays.asList(Pair.of(input1, 0.3), Pair.of(input2, 0.5))
-    );
+    Neuron hidden1 = new Neuron.Builder()
+      .withActivationFunction(ActivationFunctions::sigmoid)
+      .takesInput(input1, 0.8)
+      .takesInput(input2, 0.2)
+      .build();
     
-    Neuron output = new Neuron(
-      ActivationFunctions::sigmoid,
-      Arrays.asList(Pair.of(hidden1::getOutput, 0.3), Pair.of(hidden2::getOutput, 0.5), Pair.of(hidden3::getOutput, 0.9))
-    );
+    Neuron hidden2 = new Neuron.Builder()
+      .withActivationFunction(ActivationFunctions::sigmoid)
+      .takesInput(input1, 0.4)
+      .takesInput(input2, 0.9)
+      .build();
+    
+    Neuron hidden3 = new Neuron.Builder()
+      .withActivationFunction(ActivationFunctions::sigmoid)
+      .takesInput(input1, 0.3)
+      .takesInput(input2, 0.5)
+      .build();
+    
+    Neuron output = new Neuron.Builder()
+      .withActivationFunction(ActivationFunctions::sigmoid)
+      .takesInput(hidden1, 0.3)
+      .takesInput(hidden2, 0.5)
+      .takesInput(hidden3, 0.9)
+      .build();
     
     System.out.println("Final output: " + output.getOutput());
   }
